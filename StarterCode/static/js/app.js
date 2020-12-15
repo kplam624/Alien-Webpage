1fd4f5328c752c9548e2d3da2b885057cbcc97f3
kplam624 <kplam624@gmail.com>
2020-12-15 15:17:06 -0500

    Added a table creation.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,11 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// First connect the app to the html file.
+var tbody = d3.select("tbody");
+
+// Create a loop to append a row to the table. (<tr>)
+for(var i = 0; i < 10; i++) {
+    var row = tbody.append("tr");
+}
+
