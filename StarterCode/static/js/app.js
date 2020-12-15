6578f65149ce055708dc7d88f6eafc53c81949e1
kplam624 <kplam624@gmail.com>
2020-12-15 16:09:47 -0500

    Started to adapt the base code with the table.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,6 @@
 // from data.js
 var tableData = data;
+console.log(tableData.length)
 
 // First connect the app to the html file.
 var tbody = d3.select("tbody");
@@ -7,9 +8,17 @@ var tbody = d3.select("tbody");
 // Create a loop to append a row to the table. (<tr>)
 for(var i = 0; i < 10; i++) {
     var row = tbody.append("tr");
+    // Filled each cell with text.
     for(var j = 0; j < 7; j++){
         var cell = row.append("td");
         cell.text("Filled");
     };
 };
 
+tableData.forEach(function(aliens){
+    var row = tbody.append("tr");
+
+    Object.entries(aliens).forEach(function([key,value]){
+        console.log(key,value)
+    })
+})
