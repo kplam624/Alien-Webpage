dc53c9296d128544dc9273affe4a5d7be941d6fb
kplam624 <kplam624@gmail.com>
2020-12-15 15:34:11 -0500

    Created a text for the cells.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -7,5 +7,9 @@ var tbody = d3.select("tbody");
 // Create a loop to append a row to the table. (<tr>)
 for(var i = 0; i < 10; i++) {
     var row = tbody.append("tr");
-}
+    for(var j = 0; j < 7; j++){
+        var cell = row.append("td");
+        cell.text("Filled");
+    };
+};
 
